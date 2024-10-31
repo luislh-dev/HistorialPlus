8d6d09987dd061346cad2ef789f6e9aed8b4756d
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-10-31 11:16:02 -0500

    Initial commit

---
+++ src/main/java/com/historialplus/historialplus/HistorialPlusApplication.java
@@ -0,0 +1,13 @@
+package com.historialplus.historialplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistorialPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistorialPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/historialplus/historialplus/HistorialPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.historialplus.historialplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistorialPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
