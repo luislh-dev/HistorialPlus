29beff16787a476beedf94ab8948f52e5d5630db
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-01 01:22:55 -0500

    Add JWT authentication filter to SpringSecurityConfig

--- src/main/java/com/historialplus/historialplus/auth/SpringSecurityConfig.java
+++ src/main/java/com/historialplus/historialplus/auth/SpringSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.historialplus.historialplus.auth;
 
+import com.historialplus.historialplus.auth.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,14 +14,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SpringSecurityConfig {
 
+    private AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
+        httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
-                        .anyRequest().authenticated())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+                        .anyRequest().authenticated()
+                )
+                .addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return httpSecurity.build();
     }
 }
