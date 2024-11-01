6fa14b590b0a96ceb2eb52ffc6e91702323ffaa9
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-01 15:39:11 -0500

    Remove unnecessary exception

--- src/main/java/com/historialplus/historialplus/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/historialplus/historialplus/auth/filter/JwtAuthenticationFilter.java
@@ -44,7 +44,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
-                                            FilterChain chain, Authentication authResult) throws IOException, ServletException {
+                                            FilterChain chain, Authentication authResult) throws IOException {
         String username = authResult.getName();
         int jwtExpirationInMs = 3600000;
 
