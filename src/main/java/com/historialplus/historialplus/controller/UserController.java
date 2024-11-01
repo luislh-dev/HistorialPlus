315708f2a3553685467869e039b4a61b5c5f1aca
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-01 15:34:18 -0500

    Add get user by ID endpoint

--- src/main/java/com/historialplus/historialplus/controller/UserController.java
+++ src/main/java/com/historialplus/historialplus/controller/UserController.java
@@ -2,11 +2,15 @@ package com.historialplus.historialplus.controller;
 
 import com.historialplus.historialplus.model.UserModel;
 import com.historialplus.historialplus.service.userservice.IUserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,6 +25,12 @@ public class UserController {
     @GetMapping
     public List<UserModel> list(){
         return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable UUID id){
+        Optional<UserModel> user = service.findById(id);
+        return user.isPresent() ? ResponseEntity.ok(user.get()) : ResponseEntity.notFound().build();
     }
 
 }
