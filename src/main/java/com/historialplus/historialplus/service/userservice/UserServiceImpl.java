e5f567125ca2026858f60968a3ca68743a4e63de
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-03 14:07:17 -0500

    Remove unused Timestamp and Instant imports

--- src/main/java/com/historialplus/historialplus/service/userservice/UserServiceImpl.java
+++ src/main/java/com/historialplus/historialplus/service/userservice/UserServiceImpl.java
@@ -11,8 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
