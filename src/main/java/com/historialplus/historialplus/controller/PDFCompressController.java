90034222c86451fbdd6c31487a423ff330e9dba2
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-03 21:03:02 -0500

    Refactor PDFCompressController imports and annotations

--- src/main/java/com/historialplus/historialplus/controller/PDFCompressController.java
+++ src/main/java/com/historialplus/historialplus/controller/PDFCompressController.java
@@ -1,12 +1,14 @@
 package com.historialplus.historialplus.controller;
 
 import com.historialplus.historialplus.service.iLovePDFService.PDFCompressService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/pdf")
