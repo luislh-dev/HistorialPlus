9512fe9c11ffd3b659d860e56b84582af638a369
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-16 14:13:00 -0500

    change logic for creating a hospital

--- src/main/java/com/historialplus/historialplus/dto/hospitalDTOs/request/HospitalCreateDto.java
+++ src/main/java/com/historialplus/historialplus/dto/hospitalDTOs/request/HospitalCreateDto.java
@@ -1,7 +1,5 @@
 package com.historialplus.historialplus.dto.hospitalDTOs.request;
 
-import com.historialplus.historialplus.dto.peopleDTOs.request.PeopleCreateDto;
-import com.historialplus.historialplus.dto.userDTOs.request.UserCreateDto;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +12,4 @@ public class HospitalCreateDto {
     private String email;
     private String ruc;
     private Integer stateId;
-    private UserCreateDto adminUser; // Datos del usuario administrador
-    private PeopleCreateDto adminPerson; // Datos de la persona asociada al administrador
 }
