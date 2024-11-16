4791130452109019b11854e452e2ea1c8e63ad52
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-15 23:52:50 -0500

    Refactor use toHospitalEntity

--- src/main/java/com/historialplus/historialplus/dto/hospitalDTOs/mapper/HospitalDtoMapper.java
+++ src/main/java/com/historialplus/historialplus/dto/hospitalDTOs/mapper/HospitalDtoMapper.java
@@ -1,5 +1,6 @@
 package com.historialplus.historialplus.dto.hospitalDTOs.mapper;
 
+import com.historialplus.historialplus.dto.hospitalDTOs.request.HospitalCreateDto;
 import com.historialplus.historialplus.dto.hospitalDTOs.response.HospitalResponseDto;
 import com.historialplus.historialplus.entities.HospitalEntity;
 
@@ -17,5 +18,15 @@ public class HospitalDtoMapper {
                 hospitalEntity.getRuc(),
                 hospitalEntity.getState().getName()
         );
+    }
+
+    public static HospitalEntity toHospitalEntity(HospitalCreateDto hospitalDto) {
+        HospitalEntity hospital = new HospitalEntity();
+        hospital.setAddress(hospitalDto.getAddress());
+        hospital.setName(hospitalDto.getName());
+        hospital.setPhone(hospitalDto.getPhone());
+        hospital.setEmail(hospitalDto.getEmail());
+        hospital.setRuc(hospitalDto.getRuc());
+        return hospital;
     }
 }
