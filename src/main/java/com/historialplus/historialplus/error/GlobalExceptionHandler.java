67b1afaac933a1e817afc00045f0c227d0bfec58
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-16 09:19:43 -0500

    Add GlobalExceptionHandler

---
+++ src/main/java/com/historialplus/historialplus/error/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.historialplus.historialplus.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ValidationErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error -> {
+                    if (error instanceof FieldError) {
+                        return ((FieldError) error).getField() + ": " + error.getDefaultMessage();
+                    } else {
+                        return error.getObjectName() + ": " + error.getDefaultMessage();
+                    }
+                })
+                .collect(Collectors.toList());
+
+        ValidationErrorResponse response = new ValidationErrorResponse(errors);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
+
