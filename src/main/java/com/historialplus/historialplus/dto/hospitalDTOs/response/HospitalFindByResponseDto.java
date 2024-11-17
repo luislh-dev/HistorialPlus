fa6db2a78bc9130a2b73d78e41d7e06831b9e60f
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-17 01:28:55 -0500

    Add new response dto

---
+++ src/main/java/com/historialplus/historialplus/dto/hospitalDTOs/response/HospitalFindByResponseDto.java
@@ -0,0 +1,25 @@
+package com.historialplus.historialplus.dto.hospitalDTOs.response;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HospitalFindByResponseDto {
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+    private String ruc;
+    private Integer stateId;
+
+    public HospitalFindByResponseDto(String name, String address, String phone, String email, String ruc, Integer stateId) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.ruc = ruc;
+        this.stateId = stateId;
+    }
+}
