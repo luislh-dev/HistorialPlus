0cc946a149e34ae10e19d6f9cf309c58ced161d4
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-12 07:30:27 -0500

    Add: añadido constantes para el id de los estados

---
+++ src/main/java/com/historialplus/historialplus/constants/State.java
@@ -0,0 +1,7 @@
+package com.historialplus.historialplus.constants;
+
+public class State {
+    public static final Integer ACTIVE_ID = 1;
+    public static final Integer INACTIVE_ID = 2;
+    public static final Integer DELETED_ID = 3;
+}
