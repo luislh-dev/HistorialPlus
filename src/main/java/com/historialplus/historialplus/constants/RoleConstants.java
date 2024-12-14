716c1d9e304c8a40c052439ef9156ad7b852e1dd
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-14 15:57:30 -0500

    Add: agregar constantes con el id de los roles

--- src/main/java/com/historialplus/historialplus/constants/RoleConstants.java
+++ src/main/java/com/historialplus/historialplus/constants/RoleConstants.java
@@ -2,6 +2,9 @@ package com.historialplus.historialplus.constants;
 
 public class RoleConstants {
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final Integer ADMIN_ID = 1;
+    public static final Integer MANAGEMENT_ID = 5;
+    public static final Integer DOCTOR_ID = 3;
     public static final String ROLE_USER = "ROLE_USER";
     public static final String ROLE_DOCTOR = "ROLE_DOCTOR";
     public static final String ROLE_MANAGEMENT = "ROLE_MANAGEMENT";
