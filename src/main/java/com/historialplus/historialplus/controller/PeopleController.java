f7609e29d5d4b9832e7664a125b808ddcae5bdfa
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-13 19:14:02 -0500

    Add: agregado nuevo controlador para recuperar los datos basicos de un persona por su nombre

--- src/main/java/com/historialplus/historialplus/controller/PeopleController.java
+++ src/main/java/com/historialplus/historialplus/controller/PeopleController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/people")
@@ -25,6 +22,11 @@ public class PeopleController {
     @PostMapping
     public ResponseEntity<?> save(@Valid @RequestBody PeopleCreateDto peopleCreateDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(peopleCreateDto));
+    }
+
+    @GetMapping("/getPersonName/{dni}")
+    public ResponseEntity<?> getPersonName(@PathVariable String dni) {
+        return ResponseEntity.ok(service.getPersonName(dni));
     }
 
 }
