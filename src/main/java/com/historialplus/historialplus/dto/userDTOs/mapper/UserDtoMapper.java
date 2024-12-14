a20a219d617e6aa109f8357629af312d9c4f71a6
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-14 16:04:55 -0500

    Delete: eliminar mapper sin usar

--- src/main/java/com/historialplus/historialplus/dto/userDTOs/mapper/UserDtoMapper.java
+++ src/main/java/com/historialplus/historialplus/dto/userDTOs/mapper/UserDtoMapper.java
@@ -1,36 +1,17 @@
 package com.historialplus.historialplus.dto.userDTOs.mapper;
 
-import com.historialplus.historialplus.dto.userDTOs.UserDto;
-import com.historialplus.historialplus.dto.userDTOs.request.UserCreateDto;
 import com.historialplus.historialplus.dto.userDTOs.response.UserListResponseDto;
 import com.historialplus.historialplus.dto.userDTOs.response.UserResponseDto;
 import com.historialplus.historialplus.entities.RoleEntity;
-import com.historialplus.historialplus.entities.StateEntity;
 import com.historialplus.historialplus.entities.UserEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 import static com.historialplus.historialplus.util.roles.RoleTransformer.transformRoles;
 
 @Component
 public class UserDtoMapper {
 
-    private final PasswordEncoder passwordEncoder;
-
-    public UserDtoMapper(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    // función estática para mapear UserEntity a UserDto
-    public static UserDto toDto(UserEntity userEntity) {
-        if (userEntity == null) {
-            throw new IllegalArgumentException("El userEntity no puede ser nulo");
-        }
-
-        return new UserDto(userEntity.getId(), userEntity.getUsername(), userEntity.getEmail(), userEntity.getState().getId());
-    }
+    public UserDtoMapper() {}
 
     public static UserListResponseDto toListResponseDto(UserEntity userEntity) {
         if (userEntity == null) {
@@ -48,28 +29,6 @@ public class UserDtoMapper {
         );
     }
 
-    // función estática para mapear UserCreateDto a UserEntity
-    public UserEntity toEntity(UserCreateDto userCreateDto) {
-        if (userCreateDto == null) {
-            throw new IllegalArgumentException("El userCreateDto no puede ser nulo");
-        }
-        var userEntity = new UserEntity();
-        userEntity.setUsername(userCreateDto.getName());
-        userEntity.setEmail(userCreateDto.getEmail());
-        userEntity.setPassword(passwordEncoder.encode(userCreateDto.getPassword())); // Encriptar la contraseña
-
-        // Crear un objeto StateEntity con el id del estado
-        StateEntity stateEntity = new StateEntity();
-        stateEntity.setId(userCreateDto.getStateId());
-        userEntity.setState(stateEntity);
-
-        // Crear un objeto RoleEntity con el id del rol
-        RoleEntity roleEntity = new RoleEntity();
-        roleEntity.setId(userCreateDto.getRoleId());
-        userEntity.setRoleEntities(List.of(roleEntity));
-
-        return userEntity;
-    }
 
     // función estática para mapear UserEntity a UserResponseDto
     public static UserResponseDto toResponseDto(UserEntity userEntity) {
