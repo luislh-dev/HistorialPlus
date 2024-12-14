4dc46002dc713145ab7877bfc91d01541cd7dcc8
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-14 09:50:57 -0500

    Add: anotación para validar si el usuario autenticado realmente tiene el rol de admin, a nivel de base de datos

---
+++ src/main/java/com/historialplus/historialplus/common/security/AdminOnlyAspect.java
@@ -0,0 +1,34 @@
+package com.historialplus.historialplus.common.security;
+
+import com.historialplus.historialplus.repository.UserRepository;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import static com.historialplus.historialplus.constants.RoleConstants.ROLE_ADMIN;
+
+@Aspect
+@Component
+public class AdminOnlyAspect {
+
+    private final UserRepository userRepository;
+
+
+    public AdminOnlyAspect(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Before("@annotation(AdminOnly)")
+    public void checkAdminRole() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        boolean isAdmin = userRepository.findByUsername(username)
+                .map(user -> user.getRoleEntities().stream().anyMatch(role -> role.getName().equals(ROLE_ADMIN)))
+                .orElse(false);
+        if (!isAdmin) {
+            throw new AccessDeniedException("Usuario no autorizado.");
+        }
+    }
+
+}
