de1a19cd0500265bb37ae48896a60c2792a9e924
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-19 08:55:37 -0500

    Fix: corregido la url de los controladores

--- src/main/java/com/historialplus/historialplus/typedocument/controller/TypeDocumentController.java
+++ src/main/java/com/historialplus/historialplus/typedocument/controller/TypeDocumentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/typeDocument")
+@RequestMapping("/api/documentType")
 public class TypeDocumentController {
 
     private  final ITypeDocumentService service;
--- src/main/java/com/historialplus/historialplus/typesex/controller/SexTypeController.java
+++ src/main/java/com/historialplus/historialplus/typesex/controller/SexTypeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/sexType")
+@RequestMapping("/api/sexType")
 public class SexTypeController {
     private final ISexTypeService service;
 
