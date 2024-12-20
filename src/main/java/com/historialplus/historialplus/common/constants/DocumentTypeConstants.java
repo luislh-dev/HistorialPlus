09caac9a29ad7f01c49a5745ac3c8faf9cde108a
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-19 19:21:19 -0500

    Add: constantes con las id d elos tipo de documentos

---
+++ src/main/java/com/historialplus/historialplus/common/constants/DocumentTypeConstants.java
@@ -0,0 +1,6 @@
+package com.historialplus.historialplus.common.constants;
+
+public class DocumentTypeConstants {
+    public static final Integer DNI_ID = 1;
+    public static final Integer CE_ID = 2;
+}
