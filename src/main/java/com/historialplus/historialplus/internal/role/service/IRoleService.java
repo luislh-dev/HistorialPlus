bd19c9863d7ec495ed21f47ae12a7da8ce805452
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-23 13:50:41 -0500

    Delete: eliminar mappers, dto y funciones sin usar

--- src/main/java/com/historialplus/historialplus/internal/role/service/IRoleService.java
+++ src/main/java/com/historialplus/historialplus/internal/role/service/IRoleService.java
@@ -8,5 +8,4 @@ import java.util.Optional;
 public interface IRoleService {
     List<RoleEntity> findAll();
     Optional<RoleEntity> findById(Integer id);
-    Optional<RoleEntity> finByName(String name);
 }
--- src/main/java/com/historialplus/historialplus/internal/role/service/RoleServiceImpl.java
+++ src/main/java/com/historialplus/historialplus/internal/role/service/RoleServiceImpl.java
@@ -37,16 +37,10 @@ public class RoleServiceImpl implements IRoleService {
         return roleRepository.findById(id);
     }
 
-    @Override
-    public Optional<RoleEntity> finByName(String name) {
-        return roleRepository.findByName(name);
-    }
-
     private List<RoleEntity> getNonAdminRoles() {
         return roleRepository.findAll().stream()
                 .filter(roleEntity -> !ROLE_ADMIN.equals(roleEntity.getName()))
                 .collect(Collectors.toList());
     }
-
 
 }
