c217f37c2159e75ac3e2ff6f46af0caab97636a5
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-26 15:19:55 -0500

    Refactor: Quitar la responsabilidad de la compresion a FileServiceImpl

--- src/main/java/com/historialplus/historialplus/internal/file/service/FileServiceImpl.java
+++ src/main/java/com/historialplus/historialplus/internal/file/service/FileServiceImpl.java
@@ -1,20 +1,12 @@
 package com.historialplus.historialplus.internal.file.service;
 
-import com.adobe.pdfservices.operation.pdfjobs.params.compresspdf.CompressionLevel;
-import com.historialplus.historialplus.external.IMGCompress.service.IMGCompressService;
-import com.historialplus.historialplus.external.iLovePDF.service.PDFCompressService;
 import com.historialplus.historialplus.internal.file.dto.response.FilesResponseDto;
 import com.historialplus.historialplus.internal.file.entites.FileEntity;
 import com.historialplus.historialplus.internal.file.mapper.FilesDtoMapper;
 import com.historialplus.historialplus.internal.file.repository.FileRepository;
-import com.historialplus.historialplus.util.InMemoryMultipartFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -24,14 +16,10 @@ import java.util.stream.Collectors;
 public class FileServiceImpl implements IFileService {
 
     private final FileRepository fileRepository;
-    private final PDFCompressService pdfCompressService;
-    private final IMGCompressService imgCompressService;
 
     @Autowired
-    public FileServiceImpl(FileRepository fileRepository, PDFCompressService pdfCompressService, IMGCompressService imgCompressService) {
+    public FileServiceImpl(FileRepository fileRepository) {
         this.fileRepository = fileRepository;
-        this.pdfCompressService = pdfCompressService;
-        this.imgCompressService = imgCompressService;
     }
 
     @Override
@@ -49,35 +37,7 @@ public class FileServiceImpl implements IFileService {
 
     @Override
     public FileEntity save(FileEntity fileEntity) {
-        compressFile(fileEntity);
         return fileRepository.save(fileEntity);
     }
 
-    private void compressFile(FileEntity fileEntity) {
-        int fileTypeId = fileEntity.getFileType().getId();
-        try {
-            if (fileTypeId == 1) {
-                // PDF compression
-                Path tempFile = Files.createTempFile("temp", ".pdf");
-                Files.write(tempFile, fileEntity.getUrl().getBytes());
-                String compressedUrl = pdfCompressService.compressAndUploadPDF(tempFile, CompressionLevel.LOW);
-                fileEntity.setUrl(compressedUrl);
-                Files.deleteIfExists(tempFile);
-            } else if (fileTypeId >= 2 && fileTypeId <= 5) {
-                // Image compression
-                MultipartFile multipartFile = new InMemoryMultipartFile(
-                        "file",
-                        fileEntity.getName(),
-                        "image/" + fileEntity.getFileType().getName(),
-                        fileEntity.getUrl().getBytes()
-                );
-                String compressedUrl = imgCompressService.compressAndUploadImage(multipartFile, 80);
-                fileEntity.setUrl(compressedUrl);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Error during file compression", e);
-        } catch (Exception e) {
-            throw new RuntimeException("Unexpected error during file compression", e);
-        }
-    }
 }
