d93bc3efce19798b21b07872bf612345c38be065
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-28 19:23:58 -0500

    Feat: Añadir excepcion de INTERNAL_SERVER_ERROR

--- src/main/java/com/historialplus/historialplus/error/GlobalExceptionHandler.java
+++ src/main/java/com/historialplus/historialplus/error/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.Collections;
 import java.util.List;
@@ -16,6 +17,11 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
