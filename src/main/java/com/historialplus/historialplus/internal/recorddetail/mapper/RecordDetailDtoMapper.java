c14cf211ed0ceab117da08b1cb56c10fe6970343
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-05 22:47:36 -0500

    Refactor: remover maper innecesarios

--- src/main/java/com/historialplus/historialplus/internal/recorddetail/mapper/RecordDetailDtoMapper.java
+++ src/main/java/com/historialplus/historialplus/internal/recorddetail/mapper/RecordDetailDtoMapper.java
@@ -1,10 +1,7 @@
 package com.historialplus.historialplus.internal.recorddetail.mapper;
 
-import com.historialplus.historialplus.internal.record.entites.RecordEntity;
-import com.historialplus.historialplus.internal.recorddetail.dto.request.RecordDetailCreateDto;
 import com.historialplus.historialplus.internal.recorddetail.dto.response.RecordDetailResponseDto;
 import com.historialplus.historialplus.internal.recorddetail.entites.RecordDetailEntity;
-import com.historialplus.historialplus.internal.state.entities.StateEntity;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,16 +13,5 @@ public class RecordDetailDtoMapper {
                 detailEntity.getRecord().getId(),
                 detailEntity.getState().getId()
         );
-    }
-
-    public static RecordDetailEntity toEntity(RecordDetailCreateDto detailDto, RecordEntity parentRecord) {
-        RecordDetailEntity detailEntity = new RecordDetailEntity();
-        detailEntity.setRecord(parentRecord);
-        detailEntity.setReason(detailDto.getDescription());
-        StateEntity state = new StateEntity();
-        state.setId(detailDto.getStateId() != null ? detailDto.getStateId() : 1);
-        detailEntity.setState(state);
-
-        return detailEntity;
     }
 }
