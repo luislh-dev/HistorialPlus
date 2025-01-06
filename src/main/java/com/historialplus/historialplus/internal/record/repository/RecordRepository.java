c25d4b303d709d1ebc70598f7cc9cc509209a3f0
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-06 06:40:03 -0500

    Refactor: cambiar el tipo de resultado para validar la existencia de un record

--- src/main/java/com/historialplus/historialplus/internal/record/repository/RecordRepository.java
+++ src/main/java/com/historialplus/historialplus/internal/record/repository/RecordRepository.java
@@ -4,9 +4,10 @@ import com.historialplus.historialplus.internal.record.entites.RecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RecordRepository extends JpaRepository<RecordEntity, UUID> {
-    boolean existsByPersonId(UUID personId);
+    Optional<RecordEntity> findByPersonId(UUID personId);
 }
