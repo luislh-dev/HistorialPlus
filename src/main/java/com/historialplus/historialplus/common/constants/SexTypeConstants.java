a52db3575a407f16d60433841a5b17eff84237bb
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-06 15:21:47 -0500

    Feat: añadir constantes para la id d elos tipos de genero

---
+++ src/main/java/com/historialplus/historialplus/common/constants/SexTypeConstants.java
@@ -0,0 +1,6 @@
+package com.historialplus.historialplus.common.constants;
+
+public class SexTypeConstants {
+    public static final Integer MALE_ID = 1;
+    public static final Integer FEMALE_ID = 2;
+}
