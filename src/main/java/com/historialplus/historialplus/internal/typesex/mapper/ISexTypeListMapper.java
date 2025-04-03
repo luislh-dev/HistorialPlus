3102d4d518df7707a2db949b0e460eeb77f06b2e
Jhan-Pierre <jhanpierredev@gmail.com>
2025-04-03 17:33:52 -0500

    refactor: eliminar la anotación Qualifier de SexTypeServiceImpl y marcar ISexTypeListMapper como principal

--- src/main/java/com/historialplus/historialplus/internal/typesex/mapper/ISexTypeListMapper.java
+++ src/main/java/com/historialplus/historialplus/internal/typesex/mapper/ISexTypeListMapper.java
@@ -4,7 +4,9 @@ import com.historialplus.historialplus.internal.typesex.dto.SexTypeResponseDto;
 import com.historialplus.historialplus.internal.typesex.entities.SexTypeEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.context.annotation.Primary;
 
+@Primary
 @Mapper(componentModel = "spring")
 public interface ISexTypeListMapper {
 
--- src/main/java/com/historialplus/historialplus/internal/typesex/service/SexTypeServiceImpl.java
+++ src/main/java/com/historialplus/historialplus/internal/typesex/service/SexTypeServiceImpl.java
@@ -3,7 +3,6 @@ package com.historialplus.historialplus.internal.typesex.service;
 import com.historialplus.historialplus.internal.typesex.dto.SexTypeResponseDto;
 import com.historialplus.historialplus.internal.typesex.mapper.ISexTypeListMapper;
 import com.historialplus.historialplus.internal.typesex.repository.SexTypeRepository;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +13,7 @@ public class SexTypeServiceImpl implements ISexTypeService {
     private final SexTypeRepository repository;
 	private final ISexTypeListMapper mapper;
 
-	public SexTypeServiceImpl(SexTypeRepository repository, @Qualifier("ISexTypeListMapper") ISexTypeListMapper mapper) {
+	public SexTypeServiceImpl(SexTypeRepository repository, ISexTypeListMapper mapper) {
 		this.repository = repository;
 		this.mapper = mapper;
 	}
