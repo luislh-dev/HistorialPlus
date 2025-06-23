b7ba9fcd076373f7cde9f89e6693ee1921ad3c8a
luislh-dev <luislopezhuari27@gmail.com>
2025-06-22 19:18:11 -0500

    test: Agregar la configuración de Elasticsearch para realizar pruebas y simular dependencias en las pruebas de la aplicación

--- src/test/java/com/historialplus/historialplus/HistorialPlusApplicationTests.java
+++ src/test/java/com/historialplus/historialplus/HistorialPlusApplicationTests.java
@@ -1,15 +1,31 @@
 package com.historialplus.historialplus;
 
+import com.historialplus.historialplus.audit.aspect.AuditLogAspect;
+import com.historialplus.historialplus.audit.repository.AuditLogRepository;
+import com.historialplus.historialplus.audit.service.AuditLogServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 class HistorialPlusApplicationTests {
 
+    @MockBean
+    private ElasticsearchOperations elasticsearchOperations;
+
+    @MockBean
+    private AuditLogRepository auditLogRepository;
+
+    @MockBean
+    private AuditLogServiceImpl auditLogService;
+
+    @MockBean
+    private AuditLogAspect auditLogAspect;
+
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() {}
 
 }
