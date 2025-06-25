2dd1ebc76541d76abe099d3828d70b0da0d1a420
luislh-dev <luislopezhuari27@gmail.com>
2025-06-25 08:29:08 -0500

    refactor: hacer el constructor de FileConfig privado y corregir la declaración de MAX_FILE_SIZE

--- src/main/java/com/historialplus/historialplus/config/FileConfig.java
+++ src/main/java/com/historialplus/historialplus/config/FileConfig.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 @Configuration
 public class FileConfig {
-	public static final long MAX_FILE_SIZE = 3 * 1024 * 1024;
+	private FileConfig() {}
+
+	public static final long MAX_FILE_SIZE = 3L * 1024 * 1024;
 	public static final List<String> ALLOWED_FILE_TYPES = List.of("image/jpeg", "image/png", "image/webp", "application/pdf");
 }
