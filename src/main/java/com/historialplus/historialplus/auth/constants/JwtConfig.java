4819335b098cd221992c697b3dda77e0d7360a95
luislh-dev <luislopezhuari27@gmail.com>
2025-06-25 08:20:34 -0500

    fix: hacer el constructor de JwtConfig privado para evitar instanciación

--- src/main/java/com/historialplus/historialplus/auth/constants/JwtConfig.java
+++ src/main/java/com/historialplus/historialplus/auth/constants/JwtConfig.java
@@ -1,6 +1,8 @@
 package com.historialplus.historialplus.auth.constants;
 
 public class JwtConfig {
+    private JwtConfig(){}
+
     public static final String CONTENT_TYPE = "application/json";
     public static final String PREFIX_TOKEN = "Bearer ";
     public static final String HEADER_AUTHORIZATION = "Authorization";
