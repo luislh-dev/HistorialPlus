6ba8ded0f285e13075b85c2905612584e42eece6
luislh-dev <luislopezhuari27@gmail.com>
2025-07-05 14:16:37 -0500

    refactor(error): centralizar códigos de error en ErrorCodes

---
+++ src/main/java/com/historialplus/historialplus/error/constants/ErrorCodes.java
@@ -0,0 +1,11 @@
+package com.historialplus.historialplus.error.constants;
+
+public class ErrorCodes {
+	private ErrorCodes() {}
+
+	public static final String VALIDATION_ERROR = "VALIDATION_ERROR";
+	public static final String DATA_INTEGRITY_VIOLATION = "DATA_INTEGRITY_VIOLATION";
+	public static final String RESOURCE_NOT_FOUND = "RESOURCE_NOT_FOUND";
+	public static final String RESOURCE_CONFLICT = "RESOURCE_CONFLICT";
+
+}
