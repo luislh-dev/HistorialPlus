19e14320bde4e3982476c92a051b9013321826f6
luislh-dev <luislopezhuari27@gmail.com>
2025-07-07 00:22:49 -0500

    refactor(util): crear TimeUtils para manejo de fechas

---
+++ src/main/java/com/historialplus/historialplus/common/enums/TimeZoneConstants.java
@@ -0,0 +1,8 @@
+package com.historialplus.historialplus.common.enums;
+
+
+public class TimeZoneConstants {
+	private TimeZoneConstants() {}
+
+	public static final String LIMA = "America/Lima";
+}
---
+++ src/main/java/com/historialplus/historialplus/util/TimeUtils.java
@@ -0,0 +1,19 @@
+package com.historialplus.historialplus.util;
+
+import com.historialplus.historialplus.common.enums.TimeZoneConstants;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class TimeUtils {
+	private TimeUtils() {}
+
+	public static LocalDateTime getCurrentTime() {
+ 		return LocalDateTime.now().atZone(ZoneId.of(TimeZoneConstants.LIMA)).toLocalDateTime();
+	}
+
+	public static Instant getCurrentInstant() {
+		return Instant.now().atZone(ZoneId.of(TimeZoneConstants.LIMA)).toInstant();
+	}
+}
